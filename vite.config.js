e3c2802322151bae796cd108e69daaceddeef17d
edgardavidcure <102256702+edgardavidcure@users.noreply.github.com>
2023-07-10 14:10:27 -0700

    added page route on viteconfig

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,8 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
+        country: resolve(__dirname, "src/countryPages/index.html")
+
       },
     },
   },
